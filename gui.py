450bff90f4487e6a19ab5cc16b79097fad55eebb
Carl <cvrlix@gmail.com>
2025-07-04 11:32:02 -0400

    formatted output

--- gui.py
+++ gui.py
@@ -48,10 +48,10 @@ class App:
         self.__searching()
         self.dropdown_menu.bind('<<ComboboxSelected>>', lambda _: self.search.event_generate('<Return>', when='head'))
 
-        self.add_execute = Button(self.root, text='execute', command=lambda: self.__execute())
+        self.add_execute = Button(self.root, text='execute', command=lambda: self.__execute(), width=8)
         self.add_execute.grid(row=2, sticky='s')
 
-        self.update_list = Button(self.root, text='update')
+        self.update_list = Button(self.root, text='update', width=8)
         self.update_list.grid(row=3, sticky='n')
 
         self.data_frame = None
@@ -103,10 +103,22 @@ class App:
         selected_indices = self.list_box.curselection()
         selected_lots = [self.list_box.get(i) for i in selected_indices]
         print(selected_lots)
-        self.data_frame[self.data_frame['standard'].isin(selected_lots)].to_csv('output.txt')
-        #print('dataframe')
-        #print(self.data_frame[self.data_frame['standard'].isin(selected_lots)])
+        self.data_frame[self.data_frame['standard'].isin(selected_lots)].drop('expired', axis=1).to_csv('temp_data_frame.txt', index=False)
+
+        f = open('temp_data_frame.txt', 'r')
+        output = open('output.txt', 'w')
+        f.readline()
+
+        for line in f.readlines():
+            token = line.split(',')
+            token[1] = f'lot: {token[1]}'
+            token[2] = f'exp: {token[2]}'.replace('\n', '')
+            print(' '.join(token), file=output)
+
+        f.close()
+        output.close()
         webbrowser.open('output.txt')
+
 
         #also fix the it to updated when the comboboxchange
 
